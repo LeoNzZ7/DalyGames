2a76df390d83d3e961eadf802dde721fa3401567
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-09-11 21:01:41 -0300

    feat(game-of-the-day): implementar página do jogo do dia
    - Adicionar componente para exibir informações do jogo
    - Integrar com a API para buscar dados do jogo do dia
    - Implementar layout responsivo
    - Adicionar estilos e animações para melhorar a experiência do usuário
    - Incluir seções para plataformas, categorias e data de lançamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
+import { Container } from "@/components/Container";
+import { GameProps } from "@/utils/types/game";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getDalyGame() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+
+    return res.json()
+  }
+  catch (e) {
+    throw new Error("Failed to fetch data")
+  }
+}
+
+export default async function Home() {
+  const dalyGame: GameProps = await getDalyGame()
+
   return (
-    <div>
-      ...
-    </div>
+    <main className="flex" >
+      <Container >
+        <h1
+          className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo para você
+        </h1>
+        <Link href={`/game/${dalyGame.id}`} >
+          <section className="w-full bg-black rounded-lg" >
+            <Image
+              src={dalyGame.image_url}
+              alt={dalyGame.title}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+            />
+          </section>
+        </Link>
+      </Container>
+    </main>
   )
 }
